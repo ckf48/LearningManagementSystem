7437cd96e81bac68d503c2eb0ee081ceb105c208
ckf48 <3048764886@qq.com>
2019-12-02 23:13:49 +0800

    use recyclerView to replace ListView

--- app/src/main/java/com/example/learningmanagementsystem/ui/home/Fragments/CoursesFragment.java
+++ app/src/main/java/com/example/learningmanagementsystem/ui/home/Fragments/CoursesFragment.java
@@ -1,22 +1,18 @@
 package com.example.learningmanagementsystem.ui.home.Fragments;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.learningmanagementsystem.LearningActivity;
 import com.example.learningmanagementsystem.R;
 import com.example.learningmanagementsystem.n_ui.Course;
 
@@ -28,25 +24,26 @@ public class CoursesFragment extends Fragment {
     private String[] data = {"English", "Math", "Physics", "Biology"};
     private List<Course> courses = new ArrayList<>();
 
-    private ListView listView;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_main_coures, container, false);
-        listView = view.findViewById(R.id.course_list);
         initCourse();
-        CourseAdpter courseAdpter = new CourseAdpter(getContext(), R.layout.course_item, courses);
-        listView.setAdapter(courseAdpter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent();
-                intent.setClass(getContext(), LearningActivity.class);
-                startActivity(intent);
-            }
-        });
-
+//        ListView listView = view.findViewById(R.id.course_list);
+//        CourseAdpter courseAdpter = new CourseAdpter(getContext(), R.layout.course_item, courses);
+//        listView.setAdapter(courseAdpter);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent();
+//                intent.setClass(getContext(), LearningActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        RecyclerView recyclerView = view.findViewById(R.id.course_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        CourseAdapter courseAdapter = new CourseAdapter();
+        recyclerView.setAdapter(courseAdapter);
         return view;
     }
 
@@ -57,25 +54,57 @@ public class CoursesFragment extends Fragment {
         }
     }
 
-    private class CourseAdpter extends ArrayAdapter {
-        private int resource;
-
-        public CourseAdpter(@NonNull Context context, int resource, @NonNull List objects) {
-            super(context, resource, objects);
-            this.resource = resource;
-        }
-
+    private class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
 
         @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            Course course = (Course) getItem(position);
-            View view = LayoutInflater.from(this.getContext()).inflate(resource, null);
-            ImageView imageView = view.findViewById(R.id.course_item_image);
-            imageView.setImageResource(R.drawable.ic_launcher_background);
-            TextView textView = view.findViewById(R.id.course_item_name);
-            textView.setText(course.getName());
-            return view;
+        public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_item, parent, false);
+            return new CourseViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+            holder.name.setText(courses.get(position).getName());
+            holder.image.setImageResource(R.drawable.ic_launcher_background);
+            //TODO:get more picture resources...
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return courses.size();
+        }
+
+
+        //        private int resource;
+//
+//        public CourseAdpter(@NonNull Context context, int resource, @NonNull List objects) {
+//            super(context, resource, objects);
+//            this.resource = resource;
+//        }
+//
+//
+//        @NonNull
+//        @Override
+//        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//            Course course = (Course) getItem(position);
+//            View view = LayoutInflater.from(this.getContext()).inflate(resource, null);
+//            ImageView imageView = view.findViewById(R.id.course_item_image);
+//            imageView.setImageResource(R.drawable.ic_launcher_background);
+//            TextView textView = view.findViewById(R.id.course_item_name);
+//            textView.setText(course.getName());
+//            return view;
+//        }
+        private class CourseViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+
+            CourseViewHolder(@NonNull View itemView) {
+                super(itemView);
+                name = itemView.findViewById(R.id.course_item_name);
+                image = itemView.findViewById(R.id.course_item_image);
+            }
         }
     }
 }
